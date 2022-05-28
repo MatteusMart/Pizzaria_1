bbddb15c315d1d026d46f3707b69242226d04b54
MateusMAART <101677318+MatteusMart@users.noreply.github.com>
2022-05-27 21:17:06 -0300

    Animação adicionada

--- js/scripts.js
+++ js/scripts.js
@@ -29,11 +29,35 @@
             if(direcao =="down"){
                 $('div.folha1').addClass('animate__animated animate__fadeInUp')
                 $('div.folha3').addClass('animate__animated animate__fadeInUp')
+                $('div.camarao').addClass('animate__animated animate__fadeInUp')
+                $('div.tomate').addClass('animate__animated animate__fadeInUp')
+                $('div.folha2').addClass('animate__animated animate__fadeInUp')
+                $('div.queijo').addClass('animate__animated animate__fadeInUp')
             } if(direcao =="up"){
                 $('div.folha1').removeClass('animate__animated animate__fadeInUp')
                 $('div.folha3').removeClass('animate__animated animate__fadeInUp')
+                $('div.camarao').removeClass('animate__animated animate__fadeInUp')
+                $('div.tomate').removeClass('animate__animated animate__fadeInUp')
+                $('div.folha2').removeClass('animate__animated animate__fadeInUp')
+                $('div.queijo').removeClass('animate__animated animate__fadeInUp')
             }
         },{
             //offset= indica a quantos pixels antes do elemento começará a ser aplicado
             offset:'450px;'
         })
+
+    $('div#transicao-testemunha-plano').waypoint(
+        function(direcao){
+            if(direcao =="down"){
+                $('div#decoracao1').addClass('animate__animated animate__fadeInLeft')
+                $('div#decoracao2').addClass('animate__animated animate__fadeInUp')
+                $('div#decoracao3').addClass('animate__animated animate__fadeInRight')
+            }if(direcao =="up"){
+                $('div#decoracao1').removeClass('animate__animated animate__fadeInLeft')
+                $('div#decoracao1').removeClass('animate__animated animate__fadeInUp')
+                $('div#decoracao3').removeClass('animate__animated animate__fadeInRight')
+            }
+        },{
+            offset:'550px;'
+        }
+    )
